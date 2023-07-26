421d7a1ad405f243ff5b048af10013c70dbb9ee2
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-07-27 02:18:06 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const HomePage = async () => {
-  try {
-    const allUsers = await prisma.user.findMany();
-    console.log(allUsers);
-  } catch (error) {
-    console.error('Error fetching records:', error);
-  } 
+ 
+   
+
+
 
     return (
       <div className=" bg-center h-screen overflow-hidden" >
       <p className="text-red-500">uwu</p>
-         
+   
     </div>
     
     );
